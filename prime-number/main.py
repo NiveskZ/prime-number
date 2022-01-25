24549f6c56252d066599ba81c439a0c530cd5212
NiveskZ <83136950+NiveskZ@users.noreply.github.com>
2022-01-25 14:25:07 -0300

    Add files via upload

---
+++ prime-number/main.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+# %%
+# Start from 2, because that the first prime number.
+benchmark_comparisons = 0
+
+prime_numbers = [2]
+
+for i in range(3, 1000, 2):
+
+    # skip all numbers already divisible by any prime number already identified.
+    is_prime_number = True
+
+    for prime_number in prime_numbers:
+        benchmark_comparisons = benchmark_comparisons + 1
+        if i % prime_number == 0:
+            is_prime_number = False
+
+    if is_prime_number:
+        prime_numbers.append(i)
+
+
+print(prime_numbers)
+print(benchmark_comparisons)
+# %%
